c744b38eb8d774dc716b71f3ea2cd25d1763cada
S2010454017 <s2010454017@students.fh-hagenberg.at>
2022-12-19 16:00:27 +0100

    add falsly ignored files

---
+++ ALNS/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using HeuristicLab.PluginInfrastructure;
+
+namespace ALNS {
+  [Plugin("ALNS", "1.0")]
+  [PluginFile("ALNS.dll", PluginFileType.Assembly)]
+
+  [PluginDependency("HeuristicLab.Analysis", "3.3")]
+  [PluginDependency("HeuristicLab.Collections", "3.3")]
+  [PluginDependency("HeuristicLab.Common", "3.3")]
+  [PluginDependency("HeuristicLab.Core", "3.3")]
+  [PluginDependency("HeuristicLab.Data", "3.3")]
+  [PluginDependency("HeuristicLab.Operators", "3.3")]
+  [PluginDependency("HeuristicLab.Optimization", "3.3")]
+  [PluginDependency("HeuristicLab.Parameters", "3.3")]
+  [PluginDependency("HeuristicLab.Attic", "1.0")]
+  [PluginDependency("HeuristicLab.Random", "3.3")]
+  [PluginDependency("HeuristicLab.Selection", "3.3")]
+  [PluginDependency("HeuristicLab.Problems.Instances", "3.3")]
+  [PluginDependency("HeuristicLab.Problems.VehicleRouting", "3.4")]
+  public class Plugin : PluginBase {
+  }
+}
